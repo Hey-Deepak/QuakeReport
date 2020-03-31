74b36e360f1a936676ef34212b7482a7552a4f3f
Hey-Deepak <choudharydeepak990@gmail.com>
2020-03-31 14:43:47 +0530

    Create EarthquakeAdapter.java

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -0,0 +1,44 @@
+package com.example.android.quakereport;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    public EarthquakeAdapter(Activity context, ArrayList<Earthquake> earthquake) {
+        super(context, 0, earthquake);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.earthquake_list_item, parent, false);
+        }
+
+
+        Earthquake currentEarthquake = getItem(position);
+
+        TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude);
+
+        magnitudeTextView.setText(currentEarthquake.getMagnitude());
+
+
+        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
+
+        locationTextView.setText(currentEarthquake.getLocation());
+
+        TextView dateTextView = (TextView) listItemView.findViewById(R.id.date);
+        dateTextView.setText(currentEarthquake.getDate());
+
+        return listItemView;
+    }
+}
